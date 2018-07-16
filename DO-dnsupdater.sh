15f2d2f020a3e7b6565a04e6a2dacc1061311e19
tomchris <tom.philly@gmail.com>
2018-07-16 17:19:46 -0400

    Fixed Docker file to correct curl CA issue, Added to debugging of DO-dnsupdater.sh

--- DO-dnsupdater.sh
+++ DO-dnsupdater.sh
@@ -17,6 +17,7 @@ do
       record_id=$(curl --silent --request GET --header "Content-Type: application/json" --header "Authorization: Bearer $token" "https://api.digitalocean.com/v2/domains/$domain/records" | jq ".[] | . [] | select(.name==\"${subdomain}\")" 2>/dev/null | grep "id" | sed -E "s/[^0-9]//g")
       if [[ -z $record_id ]]; then
 	echo -e "$subdomain.$domain could not be found!!!\nCheck name or add record within DNS provider"
+        echo -e "File: $i \nDomain: $subdomain.$domain \nIP: $ip \nRecord_id: $record_id"
       else
         curl --silent -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"data":"'$ip'"}' "https://api.digitalocean.com/v2/domains/$domain/records/$record_id" > /dev/null;
         echo -e "\n== DNS updated ==\nDomain: $subdomain.$domain\nIP: $ip"
--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,15 @@ FROM ubuntu:18.04
 MAINTAINER Tom Christopoulos <tom.christopoulos@platform9.com>
 
 ENV DEBIAN_FRONTEND=noninteractive \
-    USER=root \
-    REPO_DIR=/
+    USER=root
 
 RUN apt-get update && \
-    apt-get install -y --no-install-recommends \
+    apt-get install -y \
     curl \
     jq
 
 COPY DO-dnsupdater.sh /DO-dnsupdater.sh
-COPY DO-ENV /DO-ENV
+COPY *.dns /
 
 RUN chmod 0700 /DO-*
 CMD ["/bin/bash","/DO-dnsupdater.sh"] 
