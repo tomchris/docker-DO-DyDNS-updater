27c8b05d12d4879d3c91e36b4c3548608c56c530
Tom Christopoulos <tom.christopoulos@platform9.com>
2018-07-16 13:33:40 -0700

    Updated and tested script, Modified vpn.tomchris.net.dns to use correct subdomain

--- DO-dnsupdater.sh
+++ DO-dnsupdater.sh
@@ -2,9 +2,25 @@
 
 for i in $(ls *.dns) 
 do
-    source $i 
+  token=""
+  domain=""
+  subdomain=""
+  source $i 
+  if [[ -z $token || -z $domain || -z $subdomain ]]; then
+    echo "UPDATE FOR $i FAILED!!!!"
+    echo "Environment variables missing!!!"
+  else
     ip=$(curl --silent ipinfo.io/ip)  # Obtain Public IP of this box
-    record_id=$(curl --silent --request GET --header "Content-Type: application/json" --header "Authorization: Bearer $token" "https://api.digitalocean.com/v2/domains/$domain/records" | jq ".[] | . [] | select(.name==\"${subdomain}\")" 2>/dev/null | grep "id" | sed -E "s/[^0-9]//g")
-    curl --silent -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"data":"'$ip'"}' "https://api.digitalocean.com/v2/domains/$domain/records/$record_id" > /dev/null;
-    echo -e "\n== DNS updated with IP: $ip =="
+    if [[ -z $ip ]]; then
+      echo "IP for Domain: $subdomain.$domain could not be obtained"
+    else
+      record_id=$(curl --silent --request GET --header "Content-Type: application/json" --header "Authorization: Bearer $token" "https://api.digitalocean.com/v2/domains/$domain/records" | jq ".[] | . [] | select(.name==\"${subdomain}\")" 2>/dev/null | grep "id" | sed -E "s/[^0-9]//g")
+      if [[ -z $record_id ]]; then
+	echo -e "$subdomain.$domain could not be found!!!\nCheck name or add record within DNS provider"
+      else
+        curl --silent -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"data":"'$ip'"}' "https://api.digitalocean.com/v2/domains/$domain/records/$record_id" > /dev/null;
+        echo -e "\n== DNS updated ==\nDomain: $subdomain.$domain\nIP: $ip"
+      fi
+    fi
+  fi
 done
